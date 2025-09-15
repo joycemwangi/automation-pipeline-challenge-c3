d5538d134382094c53f0dbc07b1531be4d3282ab
Joyce Mwangi <wanjajoyce@yahoo.com>
2025-09-15 04:13:14 +0300

    chore: configure Node.js globals for ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -7,10 +7,19 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        require: "readonly",
+        module: "readonly"
+      }
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: "error",
     },
     rules: {
       semi: "error",
-      quotes: ["error", "double"],
-    },
+      quotes: ["error", "double"]
+    }
   },
 ];
