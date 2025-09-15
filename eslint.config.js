b54fb67b25e47731fe9c55ac6aae1b9467cc9cb6
Joyce Mwangi <wanjajoyce@yahoo.com>
2025-09-15 03:51:36 +0300

    chore: add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: "error",
+      quotes: ["error", "double"],
+    },
+  },
+];
